c772089d82f27e2b5c20fd874279544d1272998a
dpillay03 <dpillay03@gmail.com>
2020-08-22 22:10:23 -1000

    data renduring to page

---
+++ src/data/items.js
@@ -0,0 +1,11 @@
+const Products = [
+    {
+        title: "The Sock Dock",
+        image: "https://lh3.googleusercontent.com/CYE3CP07BeShqCkOAM7ni1FSZKph6579WcJ2Lxt0RcSOw0RSS-JO9B1lfs45_CuBePkGuR3Lc7a5JY315tSVXK2cZz0sWilspJRzXJEIXG3dV-ywMjJwnWxm_PcXU0xl0SQH0fMPYPjn8csA_nU3L9uJds3x6fV8sz44TjTYCrQmhxQyDmjiL0PSw598Nhr5OGwbM6xSKj48xqGHPw7xBBYPv4HipcNqGhJrCJVe0dXUlVlFYi1cx_4vQrNQbv2HeL9uM_26sHgS2wHA2H14jWgk793adpme5qZa5783qZd9xX5zZ2gbscyQhvM1ERW7bhMWfeWetNe32THleYiF40RuEHTyW4iFiiHS3WDEl8Uck55HOav1TOpK1j57Ny74jC8MWqj5KjgQhVELt9b73O0Mb2y1EogPYL1xJ3pKMilymiy8UJL1n6ro62Gyj9ON8UZ6bqFIhLvF8NhRYZ8BkX4D5kdLqQ6YOd63f57UaDydglmYThCs0sKRWsbePkGZogepLjtzGVSxvayzKJl3-X-to6OQpYletHc5RXV2H2Ysb_-ol7I9dSbj12TBSSdnZ5TMQhiqqSIZHt47i1CijKulIyoTjz6bpBurp7zs6im1sw0Au2DOIvAY0eo0BXODuL5SkK9--gKNwMy3OfQC05rAiMwvG7qs_WSKKFduPqiVw0mqBjUa8vkJK28P=w732-h734-no?authuser=0",
+        description: "We've all been there. 5 pairs of socks enter your dryer and only 4.5 pairs return. Very few things in life are as confusing and frustrating as this moment. Worry no more, with this Sock Dock laundry tool you can ensure no sock is left behind.",
+        price: "24.95",
+        link: "https://amzn.to/2FFBse7",
+    },
+]
+
+module.exports = Products;
