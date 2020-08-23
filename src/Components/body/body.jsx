b1729bdd7d2dabf7e28a264cca42bf0ffd51b14c
dpillay03 <dpillay03@gmail.com>
2020-08-23 11:37:54 -1000

    data is styled

--- src/Components/body/body.jsx
+++ src/Components/body/body.jsx
@@ -5,24 +5,22 @@ import Products from '../../data/items.js'
 export default class Body extends React.Component {
     render() {
         return <React.Fragment>
-            <div className="project-container">
-                {Products.map((data, key) => {
-                    return (
-                        <div className="product" key={key}>
-                            <div className="product-container">
-                                <h2 className="product-title">{data.title}</h2>
-                                <a href={data.link}><img className="product-image" src={data.image} alt="product images" /></a>
-                                <p className="product-description">{data.description}</p>
-                                <div className="sub-cat-container">
-                                    <img className="product-favorite" src={Favorites} alt="product favorite" />
-                                    <p className="product-cost">${data.price}</p>
-                                    <a href={data.link}><button className="product-link">Show me!</button></a>
-                                </div>
-                            </div>
+                        <div className="product-container">
+                            {Products.map((data, key) => {
+                                return (
+                                    <div className="product" key={key}>
+                                        <h2 className="product-title">{data.title}</h2>
+                                        <a href={data.link}><img className="product-image" src={data.image} alt="product images" /></a>
+                                        <p className="product-description">{data.description}</p>
+                                        <div className="sub-cat-container">
+                                            <img className="product-favorite" src={Favorites} alt="product favorite" />
+                                            <p className="product-cost">{data.price}</p>
+                                            <a href={data.link}><button className="product-link">Show me!</button></a>
+                                        </div>
+                                    </div>
+                                );
+                            })}
                         </div>
-                    );
-                })}
-            </div>
         </React.Fragment>
     }
 }
--- src/data/items.js
+++ src/data/items.js
@@ -1,11 +1,29 @@
 const Products = [
     {
-        title: "The Sock Dock",
-        image: "https://lh3.googleusercontent.com/CYE3CP07BeShqCkOAM7ni1FSZKph6579WcJ2Lxt0RcSOw0RSS-JO9B1lfs45_CuBePkGuR3Lc7a5JY315tSVXK2cZz0sWilspJRzXJEIXG3dV-ywMjJwnWxm_PcXU0xl0SQH0fMPYPjn8csA_nU3L9uJds3x6fV8sz44TjTYCrQmhxQyDmjiL0PSw598Nhr5OGwbM6xSKj48xqGHPw7xBBYPv4HipcNqGhJrCJVe0dXUlVlFYi1cx_4vQrNQbv2HeL9uM_26sHgS2wHA2H14jWgk793adpme5qZa5783qZd9xX5zZ2gbscyQhvM1ERW7bhMWfeWetNe32THleYiF40RuEHTyW4iFiiHS3WDEl8Uck55HOav1TOpK1j57Ny74jC8MWqj5KjgQhVELt9b73O0Mb2y1EogPYL1xJ3pKMilymiy8UJL1n6ro62Gyj9ON8UZ6bqFIhLvF8NhRYZ8BkX4D5kdLqQ6YOd63f57UaDydglmYThCs0sKRWsbePkGZogepLjtzGVSxvayzKJl3-X-to6OQpYletHc5RXV2H2Ysb_-ol7I9dSbj12TBSSdnZ5TMQhiqqSIZHt47i1CijKulIyoTjz6bpBurp7zs6im1sw0Au2DOIvAY0eo0BXODuL5SkK9--gKNwMy3OfQC05rAiMwvG7qs_WSKKFduPqiVw0mqBjUa8vkJK28P=w732-h734-no?authuser=0",
-        description: "We've all been there. 5 pairs of socks enter your dryer and only 4.5 pairs return. Very few things in life are as confusing and frustrating as this moment. Worry no more, with this Sock Dock laundry tool you can ensure no sock is left behind.",
-        price: "24.95",
-        link: "https://amzn.to/2FFBse7",
+        title: "DJI Mavic Mini",
+        image: "https://media.giphy.com/media/l2SpVlY9LJd9zENb2/giphy.gif",
+        description: "With the DJI Mavic Mini Drone FlyCam Quadcopter, getting amazing footage has never been easier and more affordable. Equipped with a 2.7K Camera 3-Axis Gimbal, you can wow your friends without breaking your bank account. ",
+        price: "$399",
+        link: "https://amzn.to/2Yt5DMf",
+        tag: "Tech"
     },
+    {
+        title: "The Sock Dock",
+        image: "https://live.staticflickr.com/65535/50259425603_5ebf147352_c.jpg",
+        description: "We've all been there. 5 pairs of socks enter your dryer and only 4.5 pairs return. Very few things in life are as confusing and frustrating as this moment. The Sock Dock laundry tool you can ensure no sock is left behind.",
+        price: "$24.95",
+        link: "https://amzn.to/2FFBse7",
+        tag: "Lifestyle"
+    },
+    {
+        title: "Core 12 Person Tent",
+        image: "https://live.staticflickr.com/65535/50259497828_92c15d1005_c.jpg",
+        description: "Take your party outdoors with this spacious and luxurious 12 person tent. This 16 x 11 feet tent gives you the option to build separate rooms for maximum privacy and has an advanced venting system to keep your guests comfortable.",
+        price: "$324.99",
+        link: "https://amzn.to/32lxJdh",
+        tag: "Outdoors"
+    }
+    
 ]
 
 module.exports = Products;
