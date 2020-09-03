d71a6ec532ccf8ed629b3a15d3eb4c3a39cf57d3
dpillay03 <dpillay03@gmail.com>
2020-09-02 20:52:33 -1000

    submitting what i have. I have 9 items and i am working on the header.

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -4,9 +4,13 @@ import BoreDash from '../../assets/BoreDashLogo.jpg';
 export default class Header extends React.Component {
     render() {
         return <React.Fragment>
-            <div id="logo-container">
-                <img src={BoreDash} alt="BoreDash logo" id="logo" />
-                <p>Fight boredom with boredom</p>
+            <div class="wrapper">
+                <div class="left">Menu</div>
+                <div class="central">
+                    <img src={BoreDash} alt="BoreDash logo" id="logo" />
+                    <p>Fight boredom with boredom</p>
+                </div>
+                <div class="right">Signup/Login</div>
             </div>
             <div id="color"></div>
         </React.Fragment>
--- src/data/items.js
+++ src/data/items.js
@@ -1,27 +1,84 @@
 const Products = [
     {
+        title: "History Supreme Yacht",
+        image: "https://live.staticflickr.com/65535/50300267877_afb8a8edaf_z.jpg",
+        description: "Because why not have a 4.9 billion dollar gold plated boat. The History Supreme is a 100 foot yacht which comes equipped with 10,000 KGs of solid gold and a bedroom that is made out of a meteoride. Yes, a meteoride.",
+        price: "$4,900,000,000",
+        link: "https://www.luxhabitat.ae/the-journal/top-10-most-expensive-yachts-in-the-world/",
+        tag: "Cool",
+        item: 9
+    },
+    {
+        title: "LumiLux Toilet Light",
+        image: "https://live.staticflickr.com/65535/50300074396_75f49181bd_z.jpg",
+        description: "What's cooler than a regular toilet? I toilet with lights! Okay, maybe not... but it's still unique! THe LumiLux Toilet Light gives you the option of 16 LED colors to choose from so you can wow your friends! Get yours today.",
+        price: "$14.95",
+        link: "https://amzn.to/3lEwY8h",
+        tag: "Lifestyle",
+        item: 8
+    },
+    {
+        title: "Baby Mop",
+        image: "https://live.staticflickr.com/65535/50299381123_c286448afa_z.jpg",
+        description: "Tired of your little ones lounging around with no responsibility to do chores and pay rent? With the Baby Mop, you can now put them to work and instill the quality of hard work and persistence. Baby not included.",
+        price: "$29.99",
+        link: "https://amzn.to/3gXJUlY",
+        tag: "Fun",
+        item: 7
+    },
+    {
+        title: "Complete Suture Practice Kit",
+        image: "https://live.staticflickr.com/65535/50300051961_4033b4a264_z.jpg",
+        description: "Your hours upon hours of medical expertise watching Grey's Anatomy and The Resident can now be put to the test. With this suture practice kit you can practice your skills in hopes to save lives, or atleast pretend to save lives.",
+        price: "$39.95",
+        link: "https://amzn.to/2QOzxX3",
+        tag: "Fun",
+        item: 6
+    },
+    {
+        title: "Drop Stop - Seat Filler",
+        image: "https://live.staticflickr.com/65535/50277402081_a2f10139b7_z.jpg",
+        description: "According to NASA, black holes are a place in space where gravity pulls so much that not even light can escape. This place is known as the gap in between your car seat. Fret not, the seat filler can help!",
+        price: "$20",
+        link: "https://amzn.to/3gC1go8",
+        tag: "Lifestyle",
+        item: 5
+    },
+    {
+        title: "Million Dollar Parking Space",
+        image: "https://live.staticflickr.com/65535/50276481642_9620cae980_b.jpg",
+        description: "Parking has become a hot commodity in modern times. With cities growing and populations rising, it only makes sense that companies are going to capitilize and make money where they can. But how about $1,000,000?",
+        price: "$1,000,000",
+        link: "https://www.businessinsider.com/new-york-city-welcomes-the-1-million-parking-space-2015-8#:~:text=DNAinfo%20has%20called%20the%20parking,to%20The%20Wall%20Street%20Journal",
+        tag: "Lifestyle",
+        item: 4
+    },
+    {
         title: "DJI Mavic Mini",
         image: "https://media.giphy.com/media/l2SpVlY9LJd9zENb2/giphy.gif",
         description: "With the DJI Mavic Mini Drone FlyCam Quadcopter, getting amazing footage has never been easier and more affordable. Equipped with a 2.7K Camera 3-Axis Gimbal, you can wow your friends without breaking your bank account. ",
         price: "$399",
         link: "https://amzn.to/2Yt5DMf",
-        tag: "Tech"
+        tag: "Tech",
+        item: 3
     },
     {
         title: "The Sock Dock",
         image: "https://live.staticflickr.com/65535/50259425603_5ebf147352_c.jpg",
-        description: "We've all been there. 5 pairs of socks enter your dryer and only 4.5 pairs return. Very few things in life are as confusing and frustrating as this moment. The Sock Dock laundry tool you can ensure no sock is left behind.",
+        description: "We've all been there. 5 pairs of socks enter your dryer and only 4.5 pairs return. Very few things in life are as confusing and frustrating as this moment. The Sock Dock laundry tool ensures that no sock is left behind.",
         price: "$24.95",
         link: "https://amzn.to/2FFBse7",
-        tag: "Lifestyle"
+        tag: "Lifestyle",
+        item: 2
     },
     {
-        title: "Core 12 Person Tent",
+        title: "Spacious 12 Person Tent",
         image: "https://live.staticflickr.com/65535/50259497828_92c15d1005_c.jpg",
         description: "Take your party outdoors with this spacious and luxurious 12 person tent. This 16 x 11 feet tent gives you the option to build separate rooms for maximum privacy and has an advanced venting system to keep your guests comfortable.",
         price: "$324.99",
         link: "https://amzn.to/32lxJdh",
-        tag: "Outdoors"
+        tag: "Outdoors",
+        item: 1
     }
     
 ]
